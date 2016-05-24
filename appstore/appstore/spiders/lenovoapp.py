81b6a0367517b15de23e09f9b797365e13e6e957
Weicesun <kevinsun181@gmail.com>
2016-05-24 00:37:11 -0400

    related app

--- appstore/appstore/spiders/lenovoapp.py
+++ appstore/appstore/spiders/lenovoapp.py
@@ -9,17 +9,31 @@ class HuaweiSpider(scrapy.Spider):
     start_urls = [
 	 "http://appstore.huawei.com/more/all"
     ]
-
     def parse(self, respose):
 	page = Selector(respose)
-	divs = page.xpath('//div[@class="game-info  whole"]')
+	hrefs = page.xpath('//h4[@class="title"]/a/@href')
+	for href in hrefs:
+	    url = href.extract()
+	    yield scrapy.Request(url, callback=self.parse_item)
+	    
+    def parse_item(self, respose):
+	page = Selector(respose)
+	item = AppstoreItem()
+	item['title'] = page.xpath('//ul[@class="app-info-ul nofloat"]/li/p/span[@class="title"]/text()')\
+	.extract_first().encode('utf-8')
+        item['url'] = respose.url
+        appid = re.match(r'http://.*/(.*)', item['url']).group(1)
+        item['appid'] = appid
+	item['intro'] = page.xpath('//meta[@name="description"]/@content').\
+        extract_first().encode('utf-8')
+	divs = page.xpath('//div[@class="open-info"]')
+	recomm = ""
 	for div in divs:
-		item = AppstoreItem()
-		item['title'] = div.xpath('.//h4[@class="title"]/a/text()').extract_first().\
-		encode('utf-8')
-		item['url'] = div.xpath('.//h4[@class="title"]/a/@href').extract_first()
-		appid = re.match(r'http://.*/(.*)', item['url']).group(1)
-		item['appid'] = appid
-		item['intro'] = div.xpath('.//p[@class="content"]/text()').\
+		
+		url = div.xpath('./p[@class="name"]/a/@href').extract_first()
+		recommended_appid = re.match(r'http://.*/(.*)', url).group(1)
+		name = div.xpath('./p[@class="name"]/a/text()').\
 		extract_first().encode('utf-8')
+		recomm += "{0}:{1},".format(recommended_appid, name)
+		item['recommended'] = recomm
 		yield item
